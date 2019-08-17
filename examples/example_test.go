febb89395b734dab4ab4609d10094ed4df11cc27
igor.lyubin <ilyubin@ozon.ru>
2019-08-17 03:36:16 +0300

    init examples

---
+++ examples/example_test.go
@@ -0,0 +1,25 @@
+package examples
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+
+	t.Run("folder", func(t *testing.T) {
+
+	})
+
+	t.Run("file", func(t *testing.T) {
+
+	})
+}
+
+func TestDelete(t *testing.T) {
+
+	t.Run("folder", func(t *testing.T) {
+
+	})
+
+	t.Run("file", func(t *testing.T) {
+
+	})
+}
